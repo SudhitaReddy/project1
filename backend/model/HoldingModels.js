c2401ff252fe2c64bf579bc77252e75fd6c74b62
Sudhita Reddy <sudhitareddy582@gmail.com>
2025-05-16 16:52:52 +0530

    Initial commit

---
+++ backend/model/HoldingModels.js
@@ -0,0 +1,7 @@
+const {model} = require("mongoose");
+
+const {HoldingSchema} =require('../schemas/HoldingsSchema');
+
+const HoldingsModel =new model("holding", HoldingSchema);
+
+module.exports={HoldingsModel};
